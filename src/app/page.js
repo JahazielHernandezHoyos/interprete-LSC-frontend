25da975448d90b9223be74485cffb80ac6f1473b
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2023-11-16 23:42:01 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -54,7 +54,7 @@ export default function Home() {
           modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
           delegate: "GPU"
         },
-        runningMode: "IMAGE",
+        runningMode: "VIDEO",
       });
       await gestureRecognizer.setOptions({ runningMode: "VIDEO" });
 
